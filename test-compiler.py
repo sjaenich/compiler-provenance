59300341b0ed1a8c953793cdf5cb22d233a48d6d
root <jaenich@gmail.com>
2025-08-08 16:14:22 +0200

    new test-copmiler that works

--- test-compiler.py
+++ test-compiler.py
@@ -2,56 +2,71 @@ import subprocess
 import angr
 import os
 
+
 SOURCE_FILE = "main.c"
-REFERENCE_BINARY = "reference_binary"
+REFERENCE_BINARY = "/libpng14.so.14.3.0"
 WORK_DIR = "./bin_compare"
 
 # Dictionary: {name: path_to_compiler}
 compilers = {
-    "gcc-11": "/usr/bin/gcc-11",
-    "gcc-13": "/usr/bin/gcc-13",
+    "gcc-11": "/usr/bin/arm-linux-gnueabi-gcc-11",
+    "gcc-13": "/usr/bin/arm-linux-gnueabi-gcc-13",
+    "gcc-14": "/usr/bin/arm-linux-gnueabi-gcc-14",
     "clang-12": "/usr/bin/clang-12",
     "clang-16": "/usr/bin/clang-16",
 }
 
 flags_list = [
-    ["-O0"],
-    ["-O1"],
-    ["-O2"],
-    ["-O3"],
-    ["-Os"],
-    ["-O3", "-march=native"],
-    ["-O2", "-fno-inline"],
+    # ["-O0"],
+    # ["-O1"],
+    # ["-O2"],
+    # ["-O3"],
+    ["-Os -fno-stack-protector"],
+    ["-Os"]
+    # ["-O3 -fno-stack-protector"],
+    # ["O2 -fno-inline"],
 ]
 
 def compile_source(compiler_path, flags, output_binary):
     try:
         # Would be cool if there was an script that downloads the specific compiler that we want to try.
-        cmd = [compiler_path, SOURCE_FILE, "-o", output_binary] + flags
-        subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        package_name = "libpng"
+        version = "1.4.3"
+        cmd = f"make libpng-dirclean"
+        subprocess.run(cmd,cwd='/root/buildroot/buildroot-2025.02.4', shell=True, check=True)
+        
+
+        env = os.environ.copy()
+
+        env["MY_REAL_COMPILER"]=f"{compiler_path}"
+        env["MY_EXTRA_FLAGS"]=flags[0]
+
+        cmd =  f"uv run package_builder.py {package_name} {flags} {version} {compiler_path}"
+        subprocess.run(cmd,cwd='/root/buildroot/buildroot-2025.02.4/', shell=True, check=True,stdout=subprocess.PIPE ,env=env)
         return True
     except subprocess.CalledProcessError as e:
-        print(f"Compilation failed with {compiler_path} {' '.join(flags)}")
+        print(f"Compilation failed with {compiler_path} {' '.join(flags)}", e)
         return False
 
 def compare_binaries(bin1, bin2, function_name):
-    try:
+ #   try:
         # Needs to be changed to angr 
-        proj1 = angr.Project(bin1, auto_load_libs=False)
-        proj2 = angr.Project(bin2, auto_load_libs=False)
-        proj1.analyses.CFGFast()
-        f1_address = proj1.loader.find_symbol(function_name)
-        f1 = proj1.kb.functions[f1_address]
-        result = proj1.analyses.BinDiff(proj2)
-        f2_address = [f[1] for f in bindiff.function_matches.items() if f[0]==f1_address][0]
-        func_diff = result._function_diffs[(f1_address,f2_address)]
-        difference = len([b for b in f1.blocks]) - len(func_diff.block_matches) 
-        similarity = 1 - (difference / len([b for b in f1.blocks]))
-
-        return float(similarity)
-    except Exception as e:
-        print("Error running angr BinDiff", e)
-        return 0.0
+    function_name = "png_free_data"
+    proj1 = angr.Project(bin1, auto_load_libs=False)
+    proj2 = angr.Project(bin2, auto_load_libs=False)
+    proj1.analyses.CFGFast()
+    f1_address = proj1.loader.find_symbol(function_name).rebased_addr
+    f1 = proj1.kb.functions[f1_address]
+    result = proj1.analyses.BinDiff(proj2)
+ #   f2_address = [f[1] for f in result.function_matches if f[0]==f1_address][0]
+    f2_address = proj2.loader.find_symbol(function_name).rebased_addr
+    func_diff = result.get_function_diff(f1_address,f2_address)
+    difference = len(func_diff.attributes_a) - len(func_diff.block_matches) 
+    similarity = 1 - (difference / len([b for b in f1.blocks]))
+    return float(similarity)
+    #except Exception as e:
+     #   print("Error running angr BinDiff", e)
+      #  return 0.0
 
 def get_low_hanging_fruit(bin):
     cmd = ["strings", bin, "| grep stack_chk"]
@@ -70,10 +85,10 @@ def main():
 
         for flags in flags_list:
             output_name = f"{compiler_name}_{'_'.join(f.replace('-', '') for f in flags)}"
-            output_path = os.path.join(WORK_DIR, output_name)
+            output_path = "/root/buildroot/buildroot-2025.02.4/output/build/libpng-1.4.3/.libs/libpng14.so.14.3.0"
 
             if compile_source(compiler_path, flags, output_path):
-                score = compare_binaries(output_path, REFERENCE_BINARY)
+                score = compare_binaries(output_path, REFERENCE_BINARY, None)
                 print(f"{compiler_name} {' '.join(flags)} => Similarity: {score:.4f}")
 
                 if score > best_score:
