9f0a5bbc6516706dcc2609692a79e80f3eb1b7c5
root <jaenich@gmail.com>
2026-01-03 21:04:46 +0100

    updated getting PCs and macros from source code strings

--- parsing/localizer.py
+++ parsing/localizer.py
@@ -1,14 +1,123 @@
 import sys
+from collections import defaultdict
 import z3
 from z3 import *
+from z3 import And
 import os
 from string_parser import StringParser, SourceStringEntry
 from reverse_engineering.information.information_extractor import InformationExtractor
 from superc import SuperC
 from presence_condition import PresenceCondition
+from dataclasses import dataclass
 
 
-if __name__ == "__main__":   
+
+
+
+@dataclass    
+class TreePath:
+    data: list[SourceStringEntry]
+    presence_conditions: BoolRef
+
+
+    def __add__(self, other: "TreePath") -> "TreePath":
+        if not isinstance(other, TreePath):
+            return NotImplemented
+        return TreePath(self.data + other.data, And(self.presence_conditions,other.presence_conditions))
+    def head(self) -> "TreePath":
+        return TreePath([self.data[0]],self.presence_conditions)
+
+    def tail(self) -> "TreePath":
+        return TreePath(self.data[1:], self.presence_conditions)
+    
+    def to_string(self) -> str:
+        string =""
+        for sse in self.data:
+            string += sse.content
+        return string
+
+    def to_z3(self) -> BoolRef:
+        arguments = []
+        for sse in self.data:
+            if sse.macro:
+                x = String(sse.content)
+                arguments.append(x)
+            else:
+                arguments.append(sse.content)
+        return arguments
+
+    def contains_unresolved_macro(self) -> bool:
+        for sse in self.data:
+            if sse.macro:
+                return True
+        return False
+    
+    def __len__(self) -> int:
+        return len(self.data)
+
+def get_variants(entry: TreePath, filepath, library) -> list[TreePath]:
+    variants = []
+    if len(entry.data) >1:
+        return ValueError("TreePath should only have length 1")
+    if entry.data[0].macro:
+        presence_conditions = SuperC().get_pc_and_macro_values(filepath, library, entry.data[0].line_number, entry.data[0].content)
+        for pc in presence_conditions:
+            if pc.macro == "undefined":
+                entry.presence_conditions = And(entry.presence_conditions, pc.pc)
+                variants.append(entry)
+            else:
+                new_sse = SourceStringEntry(pc.macro, pc.line, False)
+                new_tp = TreePath([new_sse], pc.pc)
+                variants.append(new_tp)
+    else:
+        variants.append(entry)
+    return variants
+
+
+def get_all(input: TreePath, outputs: list[TreePath], filepath, library) -> list[TreePath]:
+    if len(input) == 0:
+        return outputs
+    h = input.head()
+    
+    variants = get_variants(h, filepath, library)
+    tail = input.tail()
+    old_outputs = outputs.copy()
+    outputs.clear()
+    for variant in variants:
+        # print("Variant", variant)    
+        for output in old_outputs:
+            # print("Output", output)
+            outputs.append(output + variant)
+        if outputs == []:
+            outputs.append(variant)
+    
+    return get_all(tail, outputs, filepath, library)
+
+
+
+def resolve_macros(unresolved: list[TreePath]) -> list[TreePath]:
+    resolved_strings = []
+    for concat in unresolved:
+        concat_tp = TreePath(concat, True)
+        resolved_strings += get_all(concat_tp, [],filepath, library)
+    return resolved_strings
+
+def get_pc_of_normal_strings(strings: list[TreePath]) -> list[TreePath]:
+    resolved_strings = []
+    entries = SuperC().get_pc_and_macro_values(filepath, library, None, None)
+    for string in sp.strings:
+        entry = entries[0]
+        for e in entries:
+            if e.line > string.line_number:
+                break
+            entry = e
+        # entry = next((e for e in entries if e.line >= string.line_number),None)
+        string_tp = TreePath([string], entry.pc)
+        resolved_strings.append(string_tp)
+    return resolved_strings
+
+
+if __name__ == "__main__":
     
     filepath = sys.argv[1]
     binary_path = sys.argv[2]
@@ -16,49 +125,57 @@ if __name__ == "__main__":
     sp = StringParser(filepath)
     sp.extract_string_literals()
     sp.clean_string_literals()
+    # print(sp.strings_with_unresolved_macros)
     binary_strings = InformationExtractor(file_path=binary_path)
-
-        
+    resolved_strings = []
+    
     for concat in sp.strings_with_unresolved_macros:
-        arguments = []
-        for string in concat:
-            if string.macro:
-                presence_conditions = SuperC().get_pc_and_macro_values(filepath,library,string.line_number,string.macro) 
-                if len(presence_conditions) > 0:
-                    string 
-                arguments.append(x)
-            else:
-                arguments.append(string.content)
+        concat_tp = TreePath(concat, True)
+        resolved_strings += get_all(concat_tp, [],filepath, library)
 
-        for target in binary_strings.strings:
-            s = Solver()
-            s.add(Concat(arguments) == target)
-            if s.check() == sat:
-                print("This is  the model", s.model())
+    strings = []
+    entries = SuperC().get_pc_and_macro_values(filepath, library, None, None)
+    for string in sp.strings:
+        entry = entries[0]
+        for e in entries:
+            if e.line > string.line_number:
                 break
+            entry = e
+        # entry = next((e for e in entries if e.line >= string.line_number),None)
+        string_tp = TreePath([string], entry.pc)
+        resolved_strings.append(string_tp)
 
-def create_new_strings_from_concat(concat : list[SourceStringEntry], presence_conditions: list[PresenceCondition]) -> list[str]:
-    new_strings = []
-    arguments = []
-    for pc in  presence_conditions:
-        arguments.append
+    found_strings = defaultdict(list)
+    final_solver = Solver()
+    found = False
+    print("Superc is done ")
+    for string_tp in resolved_strings:
+        arguments = []
+        
+        
+        if string_tp.contains_unresolved_macro():
+            arguments = string_tp.to_z3()
+            for target in binary_strings.strings:
+                s = Solver()
+                s.add(Concat(arguments) == target)
+                if s.check() == sat:
+                    print("This is the model", s.model(), string_tp.presence_conditions)
+                    found_strings[target].append((string_tp.presence_conditions, s.model()))
+                    break
+        else:
+            string = string_tp.to_string() 
+            for target in binary_strings.strings:
+                if target == string:
+                    print("This is a ", string, string_tp.presence_conditions)
+                    found_strings[target].append((string_tp.presence_conditions, True))
+                    found = True
+                
+    if not found:
+        for string_tp in resolved_strings:
+            if not string_tp.contains_unresolved_macro():
+                final_solver.add(Not(string_tp.presence_conditions))
+                print(final_solver)
+                final_solver.add(string_tp.presence_conditions)
+                final_solver.check()
 
-@dataclass    
-class TreePath:
-    data: list[SourceStringEntry]
-
-def get_variants(e: SourceStringEntry) -> list[SourceStringEntry]:
-    pass
-
-def get_all(input: list[SourceStringEntry], outputs: list[TreePath]) -> list[TreePath]:
-    if len(input) == 0:
-        return outputs
-    head = input[0]
-    variants = get_variants(head)
-    tail = concat[1:]
-    old_outputs = outputs.copy()
-    outputs.clear()
-    for variant in variants:   
-        for output in old_outputs:
-            outputs.append(TreePath(output.data + [variant]))
-    return get_all(tail, outputs)
+    print(final_solver)
